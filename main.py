1269866d503243a2bdacaca7d178d23c998c9d89
L1N0P3B <adolfopongo1705@gmail.com>
2025-09-23 14:14:51 -0500

    login py

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def conectar_a_bd():
     conexion = None
     return conexion
 
+
+def obtener_usuario():
+    user_id = request.args.get('user_id')
+    if not user_id: redirect(url_for('home'))
+    user = None # Buscar en BD
+    return user
+
 @app.route('/')
 def home():
     return """
@@ -16,10 +23,16 @@ def home():
                 <a href="/registro">Registro</a>
             """
 
-@app.route('/registro')
+@app.route('/registro', methods=['GET','POST'])
 def registro():
     return render_template('registro.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        correo = request.form['correo']
+        password = request.form['password']
+        
+        print(correo, password)
+        
     return render_template('login.html')
